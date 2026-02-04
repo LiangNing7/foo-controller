785b8af446e8588a174537e1b4d132a823743351
LiangNing7 <liangning2277@gmail.com>
2026-02-04 20:52:34 +0800

    feat: define controller's struct

---
+++ controller.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"k8s.io/client-go/kubernetes"
+	appslisters "k8s.io/client-go/listers/apps/v1"
+	"k8s.io/client-go/tools/cache"
+	"k8s.io/client-go/tools/record"
+	"k8s.io/client-go/util/workqueue"
+
+	clientset "github.com/LiangNing7/foo-controller/pkg/generated/clientset/versioned"
+	listers "github.com/LiangNing7/foo-controller/pkg/generated/listers/foocontroller/v1alpha1"
+)
+
+const controllerAgentName = "foo-controller"
+
+const (
+	// SuccessSynced is used as part of the Event 'reason' when a Foo is synced
+	SuccessSynced = "Synced"
+	// ErrResourceExists is used as part of the Event 'reason' when a Foo fails
+	// to sync due to a Deployment of the same name already existing.
+	ErrResourceExists = "ErrResourceExists"
+
+	// MessageResourceExists is the message used for Events when a resource
+	// fails to sync due to a Deployment already existing
+	MessageResourceExists = "Resource %q already exists and is not managed by Foo"
+	// MessageResourceSynced is the message used for an Event fired when a Foo
+	// is synced successfully
+	MessageResourceSynced = "Foo synced successfully"
+	// FieldManager distinguishes this controller from other things writing to API objects
+	FieldManager = controllerAgentName
+)
+
+// Controller is the controller implementation for Foo resource.
+type Controller struct {
+	// kubeclientset is a standard kubernetes clientset.
+	kubeclientset kubernetes.Interface
+	// fooclientset is a clientset for our own API group.
+	fooclientset clientset.Interface
+
+	deploymentsLister appslisters.DeploymentLister
+	deploymentsSynced cache.InformerSynced
+	foosLister        listers.FooLister
+	foosSynced        cache.InformerSynced
+
+	// workqueue is a rate limited work queue. This is used to queue work to be
+	// processed instead of performing it as soon as a change happens. This
+	// means we can ensure we only process a fixed amount of resources at a
+	// time, and makes it easy to ensure we are never processing the same item
+	// simultaneously in two different workers.
+	workqueue workqueue.TypedRateLimitingInterface[cache.ObjectName]
+	// recorder is an event recorder for recording Event resources to the
+	// Kubernetes API.
+	recorder record.EventRecorder
+}
